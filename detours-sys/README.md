2c0e9b912fe341bd1e513614014fa43b666d257d
Andrzej Janik <vosen@vosen.pl>
2021-01-03 18:45:48 +0100

    Fix Windows ZLUDA injector (#26)
    Fix various bugs in injector and redirector, make them more robust and enable building them by default

---
+++ detours-sys/README.md
@@ -0,0 +1,34 @@
+# Detours-sys
+
+[![Crates.io](https://img.shields.io/crates/v/detours-sys.svg)](https://crates.io/crates/detours-sys)
+![maintenance-as-is](https://img.shields.io/badge/maintenance-as--is-yellow.svg)
+
+Rust bindings to [Microsoft Detours](https://github.com/Microsoft/Detours)
+
+## Usage
+
+See the [Detours Documentation](https://github.com/Microsoft/Detours/wiki) for details.
+
+## Details
+
+Use the `buildtime_generation` feature to generate bindings at build time,
+otherwise use pregenerated bindings.
+
+Note that this will require a relatively recent version of Clang be installed.
+
+## License
+
+Licensed under either of
+
+* Apache License, Version 2.0
+   ([LICENSE-APACHE](LICENSE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0>)
+* MIT license
+   ([LICENSE-MIT](LICENSE-MIT) or <http://opensource.org/licenses/MIT>)
+
+at your option.
+
+## Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
---
+++ detours-sys/build.rs
@@ -0,0 +1,54 @@
+use std::error::Error;
+
+#[cfg(not(target_os = "windows"))]
+fn main() {}
+
+#[cfg(target_os = "windows")]
+fn main() -> Result<(), Box<dyn Error>> {
+    windows::main_impl()
+}
+
+#[cfg(target_os = "windows")]
+mod windows {
+    use std::error::Error;
+
+    const CPP_FILES: [&'static str; 5] = [
+        "../ext/detours/src/creatwth.cpp",
+        "../ext/detours/src/detours.cpp",
+        "../ext/detours/src/disasm.cpp",
+        "../ext/detours/src/image.cpp",
+        "../ext/detours/src/modules.cpp",
+    ];
+
+    pub fn main_impl() -> Result<(), Box<dyn Error>> {
+        println!("cargo:rerun-if-changed=build/wrapper.h");
+        for f in &CPP_FILES {
+            println!("cargo:rerun-if-changed={}", f);
+        }
+        build_detours()
+    }
+
+    fn build_detours() -> Result<(), Box<dyn Error>> {
+        add_target_options(
+            cc::Build::new()
+                .include("../ext/detours/src")
+                .files(&CPP_FILES),
+        )
+        .try_compile("detours")?;
+        Ok(())
+    }
+
+    #[cfg(target_env = "msvc")]
+    fn add_target_options(build: &mut cc::Build) -> &mut cc::Build {
+        build
+    }
+
+    #[cfg(not(target_env = "msvc"))]
+    fn add_target_options(build: &mut cc::Build) -> &mut cc::Build {
+        build
+            .compiler("clang")
+            .cpp(true)
+            .flag("-fms-extensions")
+            .flag("-Wno-everything")
+    }
+}
