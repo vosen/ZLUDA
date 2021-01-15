09f679693bec135776380c92409f4693587ff482
Andrzej Janik <vosen@vosen.pl>
2021-01-15 01:17:44 +0100

    Prevent linker from stripping exports on Linux (#33)

--- zluda_lib/src/lib.rs
+++ zluda_lib/src/lib.rs
@@ -1,3 +1,11 @@
 pub extern crate zluda;
 
-pub use zluda::*;
+pub use zluda::cuda::*;
+
+// For some reason, on Linux linker strips out all our re-exports,
+// there's probably a cleaner solution, but for now just exporting
+// the function below stops it from doing so
+#[no_mangle]
+fn _zluda_very_bad_linker_hack() {
+    cuInit(0);
+}
