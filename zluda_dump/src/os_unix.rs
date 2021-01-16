18d46f434f7c120b840c04f50559b586917af62a
Andrzej Janik <vosen@vosen.pl>
2021-01-16 20:15:42 +0100

    Add Linux support to dumper and build everything on CI

--- zluda_dump/src/os_unix.rs
+++ zluda_dump/src/os_unix.rs
@@ -1,9 +1,15 @@
-use std::ffi::{CStr, c_void};
+use libc::{dlopen, dlsym};
+use std::ffi::{c_void, CStr};
+
+const NVCUDA_DEFAULT_PATH: &'static [u8] = b"/usr/lib/x86_64-linux-gnu/libcuda.so.1\0";
 
 pub unsafe fn load_cuda_library() -> *mut c_void {
-    todo!()
+    dlopen(
+        NVCUDA_DEFAULT_PATH.as_ptr() as *const _,
+        libc::RTLD_LOCAL | libc::RTLD_NOW,
+    )
 }
 
 pub unsafe fn get_proc_address(handle: *mut c_void, func: &CStr) -> *mut c_void {
-    todo!()
+    libc::dlsym(handle, name.as_ptr() as *const _)
 }
