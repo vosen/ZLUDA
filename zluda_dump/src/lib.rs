4a769ef39e4c0416cad80544d2aebdf26fcc9c09
Andrzej Janik <vosen@vosen.pl>
2021-01-16 02:06:30 +0100

    Dump more information about debugged kernel launches

--- zluda_dump/src/lib.rs
+++ zluda_dump/src/lib.rs
@@ -8,12 +8,14 @@ use std::{
     mem,
     os::raw::{c_int, c_uint, c_ulong, c_ushort},
     path::PathBuf,
+    rc::Rc,
     slice,
 };
 use std::{fs::File, ptr};
 
 use cuda::{CUdeviceptr, CUfunction, CUjit_option, CUmodule, CUresult, CUstream, CUuuid};
 use ptx::ast;
+use regex::Regex;
 
 #[cfg_attr(windows, path = "os_win.rs")]
 #[cfg_attr(not(windows), path = "os_unix.rs")]
@@ -61,10 +63,22 @@ macro_rules! extern_redirect_with {
 mod cuda;
 
 pub static mut LIBCUDA_HANDLE: *mut c_void = ptr::null_mut();
-pub static mut MODULES: Option<HashMap<CUmodule, HashMap<String, Vec<usize>>>> = None;
-pub static mut KERNEL_ARGS: Option<HashMap<CUfunction, (String, Vec<usize>)>> = None;
+pub static mut MODULES: Option<HashMap<CUmodule, ModuleDump>> = None;
+pub static mut KERNELS: Option<HashMap<CUfunction, KernelDump>> = None;
 pub static mut BUFFERS: Vec<(usize, usize)> = Vec::new();
 pub static mut LAUNCH_COUNTER: usize = 0;
+pub static mut KERNEL_PATTERN: Option<Regex> = None;
+
+pub struct ModuleDump {
+    content: Rc<String>,
+    kernels_args: HashMap<String, Vec<usize>>,
+}
+
+pub struct KernelDump {
+    module_content: Rc<String>,
+    name: String,
+    arguments: Vec<usize>,
+}
 
 // We are doing dlopen here instead of just using LD_PRELOAD,
 // it's because CUDA Runtime API does dlopen to open libcuda.so, which ignores LD_PRELOAD
@@ -73,6 +87,18 @@ pub unsafe fn init_libcuda_handle() {
         let libcuda_handle = os::load_cuda_library();
         assert_ne!(libcuda_handle, ptr::null_mut());
         LIBCUDA_HANDLE = libcuda_handle;
+        match env::var("ZLUDA_DUMP_KERNEL") {
+            Ok(kernel_filter) => match Regex::new(&kernel_filter) {
+                Ok(r) => KERNEL_PATTERN = Some(r),
+                Err(err) => {
+                    eprintln!(
+                        "[ZLUDA_DUMP] Env variable ZLUDA_DUMP_KERNEL is not a regex: {}",
+                        err
+                    );
+                }
+            },
+            Err(_) => (),
+        }
         eprintln!("[ZLUDA_DUMP] Initialized");
     }
 }
@@ -106,13 +132,19 @@ unsafe fn record_module_image(module: CUmodule, image: &str) {
         let ast = ptx::ModuleParser::new().parse(&mut errors, image);
         match (&*errors, ast) {
             (&[], Ok(ast)) => {
-                let kernels = ast
+                let kernels_args = ast
                     .directives
                     .iter()
                     .filter_map(directive_to_kernel)
                     .collect::<HashMap<_, _>>();
                 let modules = MODULES.get_or_insert_with(|| HashMap::new());
-                modules.insert(module, kernels);
+                modules.insert(
+                    module,
+                    ModuleDump {
+                        content: Rc::new(image.to_string()),
+                        kernels_args,
+                    },
+                );
             }
             (errs, ast) => {
                 let err_string = errs
@@ -197,11 +229,18 @@ unsafe fn cuModuleGetFunction(
         return result;
     }
     if let Some(modules) = &MODULES {
-        if let Some(kernels) = modules.get(&hmod) {
+        if let Some(module_dump) = modules.get(&hmod) {
             if let Some(kernel) = to_str(name) {
-                if let Some(args) = kernels.get(kernel) {
-                    let kernel_args = KERNEL_ARGS.get_or_insert_with(|| HashMap::new());
-                    kernel_args.insert(*hfunc, (kernel.to_string(), args.clone()));
+                if let Some(args) = module_dump.kernels_args.get(kernel) {
+                    let kernel_args = KERNELS.get_or_insert_with(|| HashMap::new());
+                    kernel_args.insert(
+                        *hfunc,
+                        KernelDump {
+                            module_content: module_dump.content.clone(),
+                            name: kernel.to_string(),
+                            arguments: args.clone(),
+                        },
+                    );
                 } else {
                     eprintln!("[ZLUDA_DUMP] Unknown kernel: {}", kernel);
                 }
@@ -258,18 +297,27 @@ pub unsafe fn cuLaunchKernel(
     ) -> CUresult,
 ) -> CUresult {
     let mut error;
-    let maybe_kernel_args = KERNEL_ARGS.as_ref().and_then(|kernels| kernels.get(&f));
-    match maybe_kernel_args {
-        Some((kernel_name, kernel_args)) => dump_arguments(
+    let dump_env = match create_dump_dir(f, LAUNCH_COUNTER) {
+        Ok(dump_env) => dump_env,
+        Err(err) => {
+            eprintln!("[ZLUDA_DUMP] {:#?}", err);
+            None
+        }
+    };
+    if let Some(dump_env) = &dump_env {
+        dump_pre_data(
+            gridDimX,
+            gridDimY,
+            gridDimZ,
+            blockDimX,
+            blockDimY,
+            blockDimZ,
+            sharedMemBytes,
             kernelParams,
-            "pre",
-            &kernel_name,
-            LAUNCH_COUNTER,
-            kernel_args,
+            dump_env,
         )
-        .unwrap_or_else(|err| eprintln!("[ZLUDA_DUMP] {:#?}", err)),
-        None => eprintln!("[ZLUDA_DUMP] Unknown kernel: {:?}", f),
-    }
+        .unwrap_or_else(|err| eprintln!("[ZLUDA_DUMP] {:#?}", err));
+    };
     error = cont(
         f,
         gridDimX,
@@ -286,28 +334,114 @@ pub unsafe fn cuLaunchKernel(
     assert_eq!(error, CUresult::CUDA_SUCCESS);
     error = cuda::cuStreamSynchronize(hStream);
     assert_eq!(error, CUresult::CUDA_SUCCESS);
-    if let Some((kernel_name, kernel_args)) = maybe_kernel_args {
+    if let Some((_, kernel_dump)) = &dump_env {
         dump_arguments(
             kernelParams,
             "post",
-            &kernel_name,
+            &kernel_dump.name,
             LAUNCH_COUNTER,
-            kernel_args,
+            &kernel_dump.arguments,
         )
         .unwrap_or_else(|err| eprintln!("[ZLUDA_DUMP] {:#?}", err));
     }
+    LAUNCH_COUNTER += 1;
     CUresult::CUDA_SUCCESS
+}
+
+#[allow(non_snake_case)]
+fn dump_launch_arguments(
+    gridDimX: u32,
+    gridDimY: u32,
+    gridDimZ: u32,
+    blockDimX: u32,
+    blockDimY: u32,
+    blockDimZ: u32,
+    sharedMemBytes: u32,
+    dump_dir: &PathBuf,
+) -> Result<(), Box<dyn Error>> {
+    let mut module_file_path = dump_dir.clone();
+    module_file_path.push("launch.txt");
+    let mut module_file = File::create(module_file_path)?;
+    write!(&mut module_file, "{}\n", gridDimX)?;
+    write!(&mut module_file, "{}\n", gridDimY)?;
+    write!(&mut module_file, "{}\n", gridDimZ)?;
+    write!(&mut module_file, "{}\n", blockDimX)?;
+    write!(&mut module_file, "{}\n", blockDimY)?;
+    write!(&mut module_file, "{}\n", blockDimZ)?;
+    write!(&mut module_file, "{}\n", sharedMemBytes)?;
+    Ok(())
+}
+
+unsafe fn should_dump_kernel(name: &str) -> bool {
+    match &KERNEL_PATTERN {
+        Some(pattern) => pattern.is_match(name),
+        None => true,
+    }
+}
+
+unsafe fn create_dump_dir(
+    f: CUfunction,
+    counter: usize,
+) -> Result<Option<(PathBuf, &'static KernelDump)>, Box<dyn Error>> {
+    match KERNELS.as_ref().and_then(|kernels| kernels.get(&f)) {
+        Some(kernel_dump) => {
+            if !should_dump_kernel(&kernel_dump.name) {
+                return Ok(None);
+            }
+            let mut dump_dir = get_dump_dir()?;
+            dump_dir.push(format!("{:04}_{}", counter, kernel_dump.name));
+            fs::create_dir_all(&dump_dir)?;
+            Ok(Some((dump_dir, kernel_dump)))
+        }
+        None => Err("Unknown kernel: {:?}")?,
+    }
+}
+
+#[allow(non_snake_case)]
+unsafe fn dump_pre_data(
+    gridDimX: ::std::os::raw::c_uint,
+    gridDimY: ::std::os::raw::c_uint,
+    gridDimZ: ::std::os::raw::c_uint,
+    blockDimX: ::std::os::raw::c_uint,
+    blockDimY: ::std::os::raw::c_uint,
+    blockDimZ: ::std::os::raw::c_uint,
+    sharedMemBytes: ::std::os::raw::c_uint,
+    kernelParams: *mut *mut ::std::os::raw::c_void,
+    (dump_dir, kernel_dump): &(PathBuf, &'static KernelDump),
+) -> Result<(), Box<dyn Error>> {
+    dump_launch_arguments(
+        gridDimX,
+        gridDimY,
+        gridDimZ,
+        blockDimX,
+        blockDimY,
+        blockDimZ,
+        sharedMemBytes,
+        dump_dir,
+    )?;
+    let mut module_file_path = dump_dir.clone();
+    module_file_path.push("module.ptx");
+    let mut module_file = File::create(module_file_path)?;
+    module_file.write_all(kernel_dump.module_content.as_bytes())?;
+    dump_arguments(
+        kernelParams,
+        "pre",
+        &kernel_dump.name,
+        LAUNCH_COUNTER,
+        &kernel_dump.arguments,
+    )?;
+    Ok(())
 }
 
 unsafe fn dump_arguments(
     kernel_params: *mut *mut ::std::os::raw::c_void,
     prefix: &str,
-    kernel: &str,
+    kernel_name: &str,
     counter: usize,
     args: &[usize],
 ) -> Result<(), Box<dyn Error>> {
     let mut dump_dir = get_dump_dir()?;
-    dump_dir.push(format!("{:04}_{}", counter, kernel));
+    dump_dir.push(format!("{:04}_{}", counter, kernel_name));
     dump_dir.push(prefix);
     if dump_dir.exists() {
         fs::remove_dir_all(&dump_dir)?;
