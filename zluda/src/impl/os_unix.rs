b2765370e5ba86f21f6c4758817b3464c52150e5
Andrzej Janik <vosen@vosen.pl>
2021-06-27 11:46:59 +0200

    Fix more bugs

--- zluda/src/impl/os_unix.rs
+++ zluda/src/impl/os_unix.rs
@@ -2,7 +2,7 @@ use std::ffi::c_void;
 use std::ptr;
 
 pub unsafe fn heap_create() -> *mut c_void {
-    ptr::null_mut()
+    usize::MAX as *mut _
 }
 
 pub unsafe fn heap_alloc(heap: *mut c_void, bytes: usize) -> *mut c_void {
