12f44094cc84d6248e8bd8b01defa6f06f0d243a
Andrzej Janik <vosen@vosen.pl>
2021-09-14 22:41:46 +0200

    Parse and test const buffers

--- hip_runtime-sys/build.rs
+++ hip_runtime-sys/build.rs
@@ -4,5 +4,6 @@ fn main() -> Result<(), VarError> {
     println!("cargo:rustc-link-lib=dylib=amdhip64");
     //println!("cargo:rustc-link-search=native=/opt/rocm/lib/");
     println!("cargo:rustc-link-search=native=/home/ubuntu/hipamd/build/lib");
+    println!("cargo:rustc-link-search=native=/home/vosen/hipamd/build/lib");
     Ok(())
 }
