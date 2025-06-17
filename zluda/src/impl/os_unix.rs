05b515e6bb7cc14fefb9e3a21d9e3327fed6e681
Violet <c01368481@gmail.com>
2025-06-17 15:00:10 -0700

    Add `integrity_check` implementation to ZLUDA (#387)

--- zluda/src/impl/os_unix.rs
+++ zluda/src/impl/os_unix.rs
@@ -11,3 +11,13 @@ pub unsafe fn heap_alloc(_heap: *mut c_void, _bytes: usize) -> *mut c_void {
 pub unsafe fn heap_free(_heap: *mut c_void, _alloc: *mut c_void) {
     todo!()
 }
+
+// TODO: remove duplication with zluda_dump
+#[link(name = "pthread")]
+unsafe extern "C" {
+    fn pthread_self() -> std::os::unix::thread::RawPthread;
+}
+
+pub(crate) fn current_thread() -> u32 {
+    (unsafe { pthread_self() }) as u32
+}
