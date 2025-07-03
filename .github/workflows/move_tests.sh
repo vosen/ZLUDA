3ef86ca083d416034cd46949bcae54a36c184c71
Andrzej Janik <vosen@vosen.pl>
2025-07-03 01:12:38 +0000

    Fix test packing

--- .github/workflows/move_tests.sh
+++ .github/workflows/move_tests.sh
@@ -7,6 +7,6 @@ ls ${TEST_EXECUTABLES_DIR}/* | sort -u | while read -r executable; do
     output=$("$executable" --list 2>/dev/null)
     exit_code=$?
     if [ $exit_code -eq 0 ] && echo "$output" | grep -q "_${SUFFIX}: test$"; then
-        mv "$executable" "${TEST_EXECUTABLES_DIR}/${SUFFIX}/"
+        mv "$executable" "${TEST_EXECUTABLES_DIR}/../${SUFFIX}/"
     fi
 done
