c55c0fa3440330eb2a9ecb54488e1c511d4392fd
Violet <c01368481@gmail.com>
2025-07-25 16:37:03 +0000

    Silence unused variable warnings
    I'd left these in originally because I'd assumed that these functions would need full implementations soon, but they're really annoying. I've fixed all the other compiler warnings as well.

--- zluda/src/impl/os_win.rs
+++ zluda/src/impl/os_win.rs
@@ -1,20 +1,3 @@
-use std::ffi::c_void;
-
-use winapi::um::heapapi::{HeapAlloc, HeapFree};
-use winapi::um::{heapapi::HeapCreate, winnt::HEAP_NO_SERIALIZE};
-
-pub unsafe fn heap_create() -> *mut c_void {
-    HeapCreate(HEAP_NO_SERIALIZE, 0, 0)
-}
-
-pub unsafe fn heap_alloc(heap: *mut c_void, bytes: usize) -> *mut c_void {
-    HeapAlloc(heap, 0, bytes)
-}
-
-pub unsafe fn heap_free(heap: *mut c_void, alloc: *mut c_void) {
-    HeapFree(heap, 0, alloc);
-}
-
 // TODO: remove duplication with zluda_dump
 #[link(name = "kernel32")]
 unsafe extern "system" {
