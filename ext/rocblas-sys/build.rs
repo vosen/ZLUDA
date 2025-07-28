8d4e873a71c79e4fe1e9cc29b2e0a1b75f2f6b16
Violet <c01368481@gmail.com>
2025-07-28 20:22:44 +0000

    Remove verbatim requirement for linking

--- ext/rocblas-sys/build.rs
+++ ext/rocblas-sys/build.rs
@@ -1,18 +1,8 @@
 use std::env::VarError;
 
 fn main() -> Result<(), VarError> {
-    if cfg!(windows) {
-        // println!("cargo:rustc-link-lib=dylib=rocblas_4");
-        // let env = env::var("CARGO_CFG_TARGET_ENV")?;
-        // if env == "msvc" {
-        //     let mut path = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
-        //     path.push("lib");
-        //     println!("cargo:rustc-link-search=native={}", path.display());
-        // } else {
-        //     println!("cargo:rustc-link-search=native=C:\\Windows\\System32");
-        // };
-    } else {
-        println!("cargo:rustc-link-lib=dylib:+verbatim=librocblas.so.4");
+    if !cfg!(windows) {
+        println!("cargo:rustc-link-lib=dylib=rocblas");
         println!("cargo:rustc-link-search=native=/opt/rocm/lib/");
     }
     Ok(())
