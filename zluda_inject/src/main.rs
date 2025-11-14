e2283ba9fda09b89a73e0e25f61b59b0c417fefb
Andrzej Janik <vosen@vosen.pl>
2025-11-14 00:29:59 -0800

    Formatting

--- zluda_inject/src/main.rs
+++ zluda_inject/src/main.rs
@@ -2,9 +2,9 @@
 #[cfg(target_os = "windows")]
 mod win;
 #[cfg(target_os = "windows")]
-mod bin;
-#[cfg(target_os = "windows")]
 mod args;
+#[cfg(target_os = "windows")]
+mod bin;
 
 #[cfg(target_os = "windows")]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
