0afd9164a36e157bdf0a54137f1c9bc4ff9d8b0e
Andrzej Janik <vosen@vosen.pl>
2025-12-15 21:13:54 +0000

    Formatting

--- zluda/src/os_win.rs
+++ zluda/src/os_win.rs
@@ -30,7 +30,8 @@ unsafe fn load_from_self_dir(
 }
 
 #[no_mangle]
-static __pfnDliNotifyHook2: zluda_windows::PfnDliHook = zluda_windows::open_already_loaded_amdhip_probe_perflibs;
+static __pfnDliNotifyHook2: zluda_windows::PfnDliHook =
+    zluda_windows::open_already_loaded_amdhip_probe_perflibs;
 
 #[no_mangle]
 static __pfnDliFailureHook2: zluda_windows::PfnDliHook = delay_load_downgrade_amdhip;
