152baeeb05ee49437cd7ce1305f5383fb95978ec
Andrzej Janik <vosen@vosen.pl>
2026-04-16 20:51:07 -0700

    work in progress

--- zluda32/src/ipc.rs
+++ zluda32/src/ipc.rs
@@ -1,13 +1,16 @@
 use cuda_types::cuda::CUerror;
 use rand::distr::{Alphanumeric, SampleString};
+use std::os::windows::io::{AsRawHandle, FromRawHandle};
+// use rkyv::util::AlignedVec;
 use std::num::NonZeroU32;
 use std::process::{Child, Command, Stdio};
 use std::ptr;
-use std::sync::OnceLock;
+use std::sync::{Mutex, OnceLock};
 use widestring::{u16str, U16CString};
 use windows::core::{Error, Owned, PCSTR};
 use windows::Win32::Foundation::*;
 use windows::Win32::System::Pipes::*;
+use zluda_server_common::{Envelope, Opcode};
 
 fn server_path() -> &'static str {
     if cfg!(debug_assertions) {
@@ -18,7 +21,7 @@ fn server_path() -> &'static str {
 }
 
 pub(crate) struct Server {
-    pipe: Owned<HANDLE>,
+    pipe: std::fs::File,
     child: Child,
 }
 
@@ -26,58 +29,59 @@ unsafe impl Sync for Server {}
 unsafe impl Send for Server {}
 
 impl Server {
+    const CONFIG: bincode::config::Configuration<
+        bincode::config::LittleEndian,
+        bincode::config::Fixint,
+    > = bincode::config::standard().with_fixed_int_encoding();
+
+    fn new(pipe: std::fs::File, child: Child) -> Self {
+        Self { pipe, child }
+    }
+
     unsafe fn start() -> Result<Self, Error> {
         let name = Alphanumeric.sample_string(&mut rand::rng(), 32);
         let pipe_path = format!("\\\\.\\pipe\\zluda-{name}\0");
-        let pipe = Owned::new(CreateNamedPipeA(
-            PCSTR(pipe_path.as_ptr()),
-            windows::Win32::Storage::FileSystem::PIPE_ACCESS_DUPLEX,
-            PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT | PIPE_REJECT_REMOTE_CLIENTS,
-            1,
-            128 * 1024,
-            1024,
-            0,
-            None,
-        )?);
+        let pipe = std::fs::File::from_raw_handle(
+            CreateNamedPipeA(
+                PCSTR(pipe_path.as_ptr()),
+                windows::Win32::Storage::FileSystem::PIPE_ACCESS_DUPLEX,
+                PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT | PIPE_REJECT_REMOTE_CLIENTS,
+                1,
+                4 * 1024,
+                4 * 1024,
+                0,
+                None,
+            )?
+            .0,
+        );
         let child = Command::new(server_path())
             .arg(&pipe_path[..pipe_path.len() - 1])
             .stdin(Stdio::null())
             .stdout(Stdio::null())
             .stderr(Stdio::null())
             .spawn()?;
-        match ConnectNamedPipe(*pipe, None) {
-            Ok(_) => Ok(Server { pipe, child }),
-            Err(e) if e.code() == ERROR_PIPE_CONNECTED.into() => Ok(Server { pipe, child }),
+        match ConnectNamedPipe(HANDLE(pipe.as_raw_handle()), None) {
+            Ok(_) => Ok(Server::new(pipe, child)),
+            Err(e) if e.code() == ERROR_PIPE_CONNECTED.into() => Ok(Server::new(pipe, child)),
             Err(e) => Err(e),
         }
     }
 
-    pub(crate) fn get() -> Result<&'static Server, CUerror> {
-        static LOCK: OnceLock<Result<Server, Error>> = OnceLock::new();
-        LOCK.get_or_init(|| unsafe { Server::start() })
+    pub(crate) fn get() -> Result<&'static Mutex<Self>, CUerror> {
+        static LOCK: OnceLock<Result<Mutex<Server>, Error>> = OnceLock::new();
+        LOCK.get_or_init(|| Ok(Mutex::new(unsafe { Server::start()? })))
             .as_ref()
             .map_err(|_| CUerror::UNKNOWN)
     }
 
-    pub(crate) fn cu_init(&self, flags: u32) -> Result<(), CUerror> {
-        let mut in_buffer = [0xffu8; 8];
-        in_buffer[..4].copy_from_slice(1u32.to_le_bytes().as_slice());
-        in_buffer[4..].copy_from_slice(flags.to_le_bytes().as_slice());
-        let mut out_buffer = [0xffu8; 4];
-        let mut read = 0;
-        unsafe {
-            TransactNamedPipe(
-                *self.pipe,
-                Some(in_buffer.as_ptr().cast()),
-                in_buffer.len() as u32,
-                Some(out_buffer.as_mut_ptr().cast()),
-                out_buffer.len() as u32,
-                &mut read,
-                None,
-            )
+    pub(crate) fn call<Out: bincode::Decode<()>>(
+        &mut self,
+        opcode: Opcode,
+        input: &impl bincode::Encode,
+    ) -> Result<Out, CUerror> {
+        bincode::encode_into_std_write(input, &mut self.pipe, Self::CONFIG)
             .map_err(|_| CUerror::UNKNOWN)?;
-        }
-        let result = u32::from_le_bytes(out_buffer);
-        NonZeroU32::new(result).map_or_else(|| Ok(()), |e| Err(CUerror(e)))
+        bincode::decode_from_std_read::<Out, _, _>(&mut self.pipe, Self::CONFIG)
+            .map_err(|_| CUerror::UNKNOWN)
     }
 }
--- zluda32/src/lib.rs
+++ zluda32/src/lib.rs
@@ -1,8 +1,8 @@
-use std::sync::{Mutex, OnceLock};
-use compio::fs::named_pipe::NamedPipeServer;
 use cuda_macros::cuda_function_declarations;
 use cuda_types::cuda::*;
 use paste::paste;
+use std::sync::{Mutex, OnceLock};
+use zluda_server_common::*;
 
 mod ipc;
 
@@ -27,11 +27,10 @@ macro_rules! implemented {
             #[allow(improper_ctypes)]
             #[allow(improper_ctypes_definitions)]
             pub unsafe extern $abi fn $fn_name ( $( $arg_id : $arg_type),* ) -> $ret_type {
-                use crate::*;
-                paste! { [< $fn_name:snake:lower >]($($arg_id),*)? };
-                Ok(())
+                paste!{ [< $fn_name:snake >] ( $( $arg_id ),* ) }
             }
         )*
+
     };
 }
 
@@ -83,43 +82,52 @@ cuda_function_declarations! {
     ]
 }
 
-pub fn cu_init(flags: u32) -> Result<(), CUerror> {
-    let server = ipc::Server::get()?;
-    server.cu_init(flags)
+pub(crate) fn cu_init(flags: u32) -> Result<(), CUerror> {
+    let mut server = ipc::Server::get()?.lock().map_err(|_| CUerror::UNKNOWN)?;
+    let output = server.call::<Envelope<cuInitOut>>(Opcode::cuInit, &cuInitIn { Flags: flags })?;
+    output.result()
 }
 
-pub fn cu_ctx_create_v2(
+pub(crate) fn cu_ctx_create_v2(
     pctx: *mut CUcontext,
     flags: ::core::ffi::c_uint,
     dev: CUdevice,
 ) -> Result<(), CUerror> {
+    let ctx = unsafe { pctx.as_mut() }.ok_or(CUerror::INVALID_VALUE)?;
+    let mut server = ipc::Server::get()?.lock().map_err(|_| CUerror::UNKNOWN)?;
+    let output = server.call::<Envelope<cuCtxCreate_v2Out>>(
+        Opcode::cuCtxCreate_v2,
+        &cuCtxCreate_v2In { flags, dev },
+    )?;
+    output.result()?;
+    *ctx = CudaEncode::decode(output.data.pctx);
+    Ok(())
+}
+
+pub(crate) fn cu_ctx_detach(ctx: CUcontext) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_ctx_detach(ctx: CUcontext) -> Result<(), CUerror> {
-    todo!()
-}
-
-pub fn cu_ctx_get_api_version(
+pub(crate) fn cu_ctx_get_api_version(
     ctx: CUcontext,
     version: *mut ::core::ffi::c_uint,
 ) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_ctx_get_current(pctx: *mut CUcontext) -> Result<(), CUerror> {
+pub(crate) fn cu_ctx_get_current(pctx: *mut CUcontext) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_ctx_get_device(device: *mut CUdevice) -> Result<(), CUerror> {
+pub(crate) fn cu_ctx_get_device(device: *mut CUdevice) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_ctx_synchronize() -> Result<(), CUerror> {
+pub(crate) fn cu_ctx_synchronize() -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_device_compute_capability(
+pub(crate) fn cu_device_compute_capability(
     major: *mut ::core::ffi::c_int,
     minor: *mut ::core::ffi::c_int,
     dev: CUdevice,
@@ -127,23 +135,49 @@ pub fn cu_device_compute_capability(
     todo!()
 }
 
-pub fn cu_device_get(device: *mut CUdevice, ordinal: ::core::ffi::c_int) -> Result<(), CUerror> {
-    todo!()
+pub(crate) fn cu_device_get(
+    device: *mut CUdevice,
+    ordinal: ::core::ffi::c_int,
+) -> Result<(), CUerror> {
+    let device = unsafe { device.as_mut() }.ok_or(CUerror::INVALID_VALUE)?;
+    let mut server = ipc::Server::get()?.lock().map_err(|_| CUerror::UNKNOWN)?;
+    let output =
+        server.call::<Envelope<cuDeviceGetOut>>(Opcode::cuDeviceGet, &cuDeviceGetIn { ordinal })?;
+    output.result()?;
+    *device = CudaEncode::decode(output.data.device);
+    Ok(())
 }
 
-pub fn cu_device_get_attribute(
+pub(crate) fn cu_device_get_attribute(
     pi: *mut ::core::ffi::c_int,
     attrib: CUdevice_attribute,
     dev: CUdevice,
 ) -> Result<(), CUerror> {
-    todo!()
+    let pi = unsafe { pi.as_mut() }.ok_or(CUerror::INVALID_VALUE)?;
+    let mut server = ipc::Server::get()?.lock().map_err(|_| CUerror::UNKNOWN)?;
+    let output = server.call::<Envelope<cuDeviceGetAttributeOut>>(
+        Opcode::cuDeviceGetAttribute,
+        &cuDeviceGetAttributeIn {
+            attrib: CudaEncode::encode(attrib),
+            dev,
+        },
+    )?;
+    output.result()?;
+    *pi = CudaEncode::decode(output.data.pi);
+    Ok(())
 }
 
-pub fn cu_device_get_count(count: *mut ::core::ffi::c_int) -> Result<(), CUerror> {
-    todo!()
+pub(crate) fn cu_device_get_count(count: *mut ::core::ffi::c_int) -> Result<(), CUerror> {
+    let count = unsafe { count.as_mut() }.ok_or(CUerror::INVALID_VALUE)?;
+    let mut server = ipc::Server::get()?.lock().map_err(|_| CUerror::UNKNOWN)?;
+    let output = server
+        .call::<Envelope<cuDeviceGetCountOut>>(Opcode::cuDeviceGetCount, &cuDeviceGetCountIn {})?;
+    output.result()?;
+    *count = CudaEncode::decode(output.data.count);
+    Ok(())
 }
 
-pub fn cu_device_get_name(
+pub(crate) fn cu_device_get_name(
     name: *mut ::core::ffi::c_char,
     len: ::core::ffi::c_int,
     dev: CUdevice,
@@ -151,34 +185,45 @@ pub fn cu_device_get_name(
     todo!()
 }
 
-pub fn cu_device_get_properties(prop: *mut CUdevprop, dev: CUdevice) -> Result<(), CUerror> {
+pub(crate) fn cu_device_get_properties(prop: *mut CUdevprop, dev: CUdevice) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_device_total_mem_v2(bytes: *mut usize, dev: CUdevice) -> Result<(), CUerror> {
+pub(crate) fn cu_device_total_mem_v2(bytes: *mut usize, dev: CUdevice) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_driver_get_version(driverVersion: *mut ::core::ffi::c_int) -> Result<(), CUerror> {
+pub(crate) fn cu_driver_get_version(driverVersion: *mut ::core::ffi::c_int) -> Result<(), CUerror> {
+    let driverVersion = unsafe { driverVersion.as_mut() }.ok_or(CUerror::INVALID_VALUE)?;
+    let mut server = ipc::Server::get()?.lock().map_err(|_| CUerror::UNKNOWN)?;
+    let output = server.call::<Envelope<cuDriverGetVersionOut>>(
+        Opcode::cuDriverGetVersion,
+        &cuDriverGetVersionIn {},
+    )?;
+    output.result()?;
+    *driverVersion = CudaEncode::decode(output.data.driverVersion);
+    Ok(())
+}
+
+pub(crate) fn cu_event_create(
+    phEvent: *mut CUevent,
+    Flags: ::core::ffi::c_uint,
+) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_event_create(phEvent: *mut CUevent, Flags: ::core::ffi::c_uint) -> Result<(), CUerror> {
+pub(crate) fn cu_event_destroy_v2(hEvent: CUevent) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_event_destroy_v2(hEvent: CUevent) -> Result<(), CUerror> {
-    todo!()
-}
-
-pub fn cu_get_export_table(
+pub(crate) fn cu_get_export_table(
     ppExportTable: *mut *const ::core::ffi::c_void,
     pExportTableId: *const CUuuid,
 ) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_launch_kernel(
+pub(crate) fn cu_launch_kernel(
     f: CUfunction,
     gridDimX: ::core::ffi::c_uint,
     gridDimY: ::core::ffi::c_uint,
@@ -194,19 +239,19 @@ pub fn cu_launch_kernel(
     todo!()
 }
 
-pub fn cu_mem_alloc_v2(dptr: *mut CUdeviceptr, bytesize: usize) -> Result<(), CUerror> {
+pub(crate) fn cu_mem_alloc_v2(dptr: *mut CUdeviceptr, bytesize: usize) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_mem_free_host(p: *mut ::core::ffi::c_void) -> Result<(), CUerror> {
+pub(crate) fn cu_mem_free_host(p: *mut ::core::ffi::c_void) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_mem_free_v2(dptr: CUdeviceptr) -> Result<(), CUerror> {
+pub(crate) fn cu_mem_free_v2(dptr: CUdeviceptr) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_mem_get_address_range_v2(
+pub(crate) fn cu_mem_get_address_range_v2(
     pbase: *mut CUdeviceptr,
     psize: *mut usize,
     dptr: CUdeviceptr,
@@ -214,7 +259,7 @@ pub fn cu_mem_get_address_range_v2(
     todo!()
 }
 
-pub fn cu_mem_host_alloc(
+pub(crate) fn cu_mem_host_alloc(
     pp: *mut *mut ::core::ffi::c_void,
     bytesize: usize,
     Flags: ::core::ffi::c_uint,
@@ -222,7 +267,7 @@ pub fn cu_mem_host_alloc(
     todo!()
 }
 
-pub fn cu_memcpy_dto_d_async_v2(
+pub(crate) fn cu_memcpy_dto_d_async_v2(
     dstDevice: CUdeviceptr,
     srcDevice: CUdeviceptr,
     ByteCount: usize,
@@ -231,7 +276,7 @@ pub fn cu_memcpy_dto_d_async_v2(
     todo!()
 }
 
-pub fn cu_memcpy_dto_h_async_v2(
+pub(crate) fn cu_memcpy_dto_h_async_v2(
     dstHost: *mut ::core::ffi::c_void,
     srcDevice: CUdeviceptr,
     ByteCount: usize,
@@ -240,7 +285,7 @@ pub fn cu_memcpy_dto_h_async_v2(
     todo!()
 }
 
-pub fn cu_memcpy_hto_d_async_v2(
+pub(crate) fn cu_memcpy_hto_d_async_v2(
     dstDevice: CUdeviceptr,
     srcHost: *const ::core::ffi::c_void,
     ByteCount: usize,
@@ -249,7 +294,7 @@ pub fn cu_memcpy_hto_d_async_v2(
     todo!()
 }
 
-pub fn cu_memset_d8_v2(
+pub(crate) fn cu_memset_d8_v2(
     dstDevice: CUdeviceptr,
     uc: ::core::ffi::c_uchar,
     N: usize,
@@ -257,7 +302,7 @@ pub fn cu_memset_d8_v2(
     todo!()
 }
 
-pub fn cu_module_get_function(
+pub(crate) fn cu_module_get_function(
     hfunc: *mut CUfunction,
     hmod: CUmodule,
     name: *const ::core::ffi::c_char,
@@ -265,7 +310,7 @@ pub fn cu_module_get_function(
     todo!()
 }
 
-pub fn cu_module_get_global_v2(
+pub(crate) fn cu_module_get_global_v2(
     dptr: *mut CUdeviceptr,
     bytes: *mut usize,
     hmod: CUmodule,
@@ -274,7 +319,7 @@ pub fn cu_module_get_global_v2(
     todo!()
 }
 
-pub fn cu_module_get_tex_ref(
+pub(crate) fn cu_module_get_tex_ref(
     pTexRef: *mut CUtexref,
     hmod: CUmodule,
     name: *const ::core::ffi::c_char,
@@ -282,18 +327,18 @@ pub fn cu_module_get_tex_ref(
     todo!()
 }
 
-pub fn cu_stream_create(
+pub(crate) fn cu_stream_create(
     phStream: *mut CUstream,
     Flags: ::core::ffi::c_uint,
 ) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_stream_destroy_v2(hStream: CUstream) -> Result<(), CUerror> {
+pub(crate) fn cu_stream_destroy_v2(hStream: CUstream) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_tex_ref_set_address_mode(
+pub(crate) fn cu_tex_ref_set_address_mode(
     hTexRef: CUtexref,
     dim: ::core::ffi::c_int,
     am: CUaddress_mode,
@@ -301,7 +346,7 @@ pub fn cu_tex_ref_set_address_mode(
     todo!()
 }
 
-pub fn cu_tex_ref_set_address_v2(
+pub(crate) fn cu_tex_ref_set_address_v2(
     ByteOffset: *mut usize,
     hTexRef: CUtexref,
     dptr: CUdeviceptr,
@@ -310,15 +355,21 @@ pub fn cu_tex_ref_set_address_v2(
     todo!()
 }
 
-pub fn cu_tex_ref_set_filter_mode(hTexRef: CUtexref, fm: CUfilter_mode) -> Result<(), CUerror> {
+pub(crate) fn cu_tex_ref_set_filter_mode(
+    hTexRef: CUtexref,
+    fm: CUfilter_mode,
+) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_tex_ref_set_flags(hTexRef: CUtexref, Flags: ::core::ffi::c_uint) -> Result<(), CUerror> {
+pub(crate) fn cu_tex_ref_set_flags(
+    hTexRef: CUtexref,
+    Flags: ::core::ffi::c_uint,
+) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_tex_ref_set_format(
+pub(crate) fn cu_tex_ref_set_format(
     hTexRef: CUtexref,
     fmt: CUarray_format,
     NumPackedComponents: ::core::ffi::c_int,
@@ -326,25 +377,28 @@ pub fn cu_tex_ref_set_format(
     todo!()
 }
 
-pub fn cu_tex_ref_set_max_anisotropy(
+pub(crate) fn cu_tex_ref_set_max_anisotropy(
     hTexRef: CUtexref,
     maxAniso: ::core::ffi::c_uint,
 ) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_tex_ref_set_mipmap_filter_mode(
+pub(crate) fn cu_tex_ref_set_mipmap_filter_mode(
     hTexRef: CUtexref,
     fm: CUfilter_mode,
 ) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_tex_ref_set_mipmap_level_bias(hTexRef: CUtexref, bias: f32) -> Result<(), CUerror> {
+pub(crate) fn cu_tex_ref_set_mipmap_level_bias(
+    hTexRef: CUtexref,
+    bias: f32,
+) -> Result<(), CUerror> {
     todo!()
 }
 
-pub fn cu_tex_ref_set_mipmap_level_clamp(
+pub(crate) fn cu_tex_ref_set_mipmap_level_clamp(
     hTexRef: CUtexref,
     minMipmapLevelClamp: f32,
     maxMipmapLevelClamp: f32,
