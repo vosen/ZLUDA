0d25248005245bbfac028d509bcb2b65bfbab8d0
Andrzej Janik <vosen@vosen.pl>
2026-05-27 21:10:28 -0700

    Fix clz.b64 and add bfind.shiftamt (#644)

--- ptx_parser/src/lib.rs
+++ ptx_parser/src/lib.rs
@@ -4414,6 +4414,14 @@ derive_parser!(
     }
 
     .type: ScalarType = { .b32, .b64 };
+
+    // https://docs.nvidia.com/cuda/parallel-thread-execution/
+    bfind.shiftamt.u32  d, a => {
+        Instruction::Bfind {
+            data: (),
+            arguments: BfindArgs { dst: d, src: a }
+        }
+    }
 );
 
 #[cfg(test)]
