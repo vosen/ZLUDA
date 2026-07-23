3be4f523cc981033932f790d8bd0364e37915293
0rlych1kk4 <orlychikka@gmail.com>
2026-07-23 21:09:37 +0800

    feat: emit extern global declarations
    Signed-off-by: 0rlych1kk4 <orlychikka@gmail.com>

--- ptx/src/pass/llvm/emit.rs
+++ ptx/src/pass/llvm/emit.rs
@@ -240,7 +240,7 @@ impl<'a, 'input> ModuleEmitContext<'a, 'input> {
 
     fn emit_global(
         &mut self,
-        _linking: ast::LinkingDirective,
+        linking: ast::LinkingDirective,
         var: ast::Variable<SpirvWord>,
     ) -> Result<(), TranslateError> {
         let name = self
@@ -266,18 +266,31 @@ impl<'a, 'input> ModuleEmitContext<'a, 'input> {
                 get_state_space(var.info.state_space)?,
             )
         };
-        if matches!(var.info.v_type, ast::Type::Texref) {
+        let is_declaration = linking.contains(ast::LinkingDirective::EXTERN);
+
+        if matches!(var.info.v_type, ast::Type::Texref) && !is_declaration {
             unsafe { LLVMSetInitializer(global, LLVMGetUndef(llvm_type)) };
             unsafe {
                 LLVMSetAlignment(global, 8);
             }
         }
-        self.emit_linkage(global, &var.info)?;
+
+        if is_declaration {
+            unsafe {
+                LLVMSetLinkage(global, LLVMLinkage::LLVMExternalLinkage);
+                LLVMSetVisibility(global, LLVMVisibility::LLVMDefaultVisibility);
+            }
+        } else {
+            self.emit_linkage(global, &var.info)?;
+        }
+
         self.resolver.register(var.name, global);
+
         if let Some(align) = var.info.align {
             unsafe { LLVMSetAlignment(global, align) };
         }
-        if !var.info.array_init.is_empty() {
+
+        if !is_declaration && !var.info.array_init.is_empty() {
             let initializer = self.get_array_init(&var.info.v_type, &*var.info.array_init)?;
             unsafe { LLVMSetInitializer(global, initializer) };
         }
